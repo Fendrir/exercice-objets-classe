4e90396db71b6bb413bbc0c5d8f9ef81bd951b66
Julien <papao1@hotmail.fr>
2017-08-07 16:47:29 +0200

    2 eme partie ok

--- app.js
+++ app.js
@@ -37,4 +37,29 @@ var julien = new Eleve('id', 'Julien', 'Febvre', "m", 28);
 julien.addAffiche();
 
 
-// function Classe()
+function Classe(){
+
+	this.curIdEleve = 1;
+	this.eleves = [];
+
+	this.addEleve = function( prenom, nom, sexe, age) {
+
+		this.eleves.push( new Eleve ( this.curIdEleve, prenom, nom, sex, age));
+		this.curIdEleve++;
+
+
+	}
+
+	this.compteEleve = function (sexe = 'A'){
+
+
+	}
+
+}
+
+var maClasse = new Classe();
+maClasse.addEleve("Pierre", "De Soos", "M", 41);
+maClasse.addEleve("Jean", "Vallat", "M", 22);
+maClasse.addEleve("Louise", "Leontine", "f", 27);
+
+console.log("Il y a " + maClasse.compteEleve('A') + " élèves dans la classe");
